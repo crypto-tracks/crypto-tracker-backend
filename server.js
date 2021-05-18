4ad94d982c282f7bfe98e6155496a211014a4f0c
Tyler Johnson <tyler@csdconsultants.us>
2021-05-17 22:05:36 -0700

    feat: connected mongoose database and started app.post

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-// const mongoose = require('mongoose'); TODO Add dependency when we start using mongoose
+const mongoose = require('mongoose'); 
 require('dotenv').config();
 
 const errorHandler = require('./handlers/error');
@@ -8,6 +8,8 @@ const getCoins = require('./handlers/coins');
 const getCoinInfo = require('./handlers/coin-info');
 const getCoinLatest = require('./handlers/coin-latest');
 const getNews = require('./handlers/news');
+
+mongoose.connect(`${MONGODB_URI}`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 app.use(express.json());
@@ -35,6 +37,27 @@ app.get('/coin-latest', getCoinLatest);
 
 // Get News
 app.get('/news', getNews);
+
+app.post('/user-coins', (req, res) => {
+  User.find({ email: req.body.email }, (err, databaseResults) => {
+    if (databaseResults.length < 1) {
+      res.status(400).send('error: user does not exist');
+    } else {
+      let user = databaseResults[0];
+      req.body.userCoins.forEach(item => {
+        //below if/else is from best books, rewrite for crypto tracker? Or not necessary?
+        if (typeof (item.name) === 'string' && typeof (item.description) === 'string' && typeof (item.status) === 'boolean') {
+          user.userCoins.push(item);
+        } else {
+          console.log('invalid entry');
+        }
+      });
+      user.save().then((databaseResults) => {
+        res.send(databaseResults.userCoins);
+      });
+    }
+  });
+});
 
 // Listen on Port
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
